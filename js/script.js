afeb77782169a9c9468f85824108fd9471b7bcff
unknown <dbearheart@gmail.com>
2022-09-20 20:00:56 -0700

    added another console.log

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-alert("it works")
+alert("it works for now!")
 
 // this is a single line comment//
 
 /* this is a 
 multi-line comment*/
-console.log("David Garza");
-console.log("All Japanese Food");
+console.log("My Name is David Garza");
+console.log("Favorite Food is all Japanese Food");
