153b1baa96412feade0767d30a11b268df9c7fd9
unknown <dbearheart@gmail.com>
2022-09-22 18:28:06 -0700

    added number & BigInt to Day 2 daily challenge

--- js/script.js
+++ js/script.js
@@ -3,6 +3,11 @@ alert("it works for now!")
 // this is a single line comment//
 
 /* this is a 
-multi-line comment*/
+multi-line comment
+for javascript 
+practice
+*/
 console.log("My Name is David Garza");
-console.log("Favorite Food is all Japanese Food");
+console.log("Favorite Food is all Japanese Food 🍙");
+console.log(typeof 34);
+console.log(typeof 34n)
